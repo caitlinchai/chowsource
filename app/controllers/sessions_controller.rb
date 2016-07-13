957e5a192b993493e1f1f2c33bb9870015501848
Brandon Wu & Caitlin Chai & Linse Mathew <pair+beewuu.caitlinchai.linsemathew@devbootcamp.com>
2016-07-13 17:54:20 -0400

    user login and authentication, add login,logout,register links to layout file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+
+  def new
+    redirect_to current_user if logged_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      login(user)
+      redirect_to user
+    else
+      flash[:notice] ="Invalid email/password combination."
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+private
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -9,7 +12,12 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    if logged_in?
+      flash[:notice]="You must logout to register a new user."
+      redirect_to current_user
+    else
+      @user = User.new
+     end
   end
 
   def create
@@ -45,5 +53,17 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:notice]="Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
