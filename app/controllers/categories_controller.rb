260d78ff3ca352efd0c0157118f6d8cbd0bde54a
Brandon Wu <email": null>
2016-07-13 15:44:29 -0400

    implement category controller functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,15 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @recipes = Recipe.where( category_id: id_params )
+  end
+
+  private
+
+  def id_params
+    params.require(:id)
+  end
 end
