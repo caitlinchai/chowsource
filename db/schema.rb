e6e3d92f45e7f39880e94d24a9dcb7fabed37851
Linse Mathew <email": null>
2016-07-13 17:17:15 -0400

    Add seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713190511) do
+ActiveRecord::Schema.define(version: 20160713201216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20160713190511) do
     t.string   "name",            null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
-    t.string   "location",        null: false
-    t.text     "about",           null: false
+    t.string   "location"
+    t.string   "about"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
