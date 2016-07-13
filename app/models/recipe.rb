553738a51189cde90423fd2c848636963576030f
Linse Mathew <email": null>
2016-07-13 12:13:07 -0400

    test database, add missing validations and associations, establish routes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :ingredients, dependent: :destroy
-
+  has_many :ratings, dependent: :destroy
   validates :name, :difficulty_level, :prep_time, :directions, presence: true
   validates :description, :creator, :servings, :category, presence: true
 end
