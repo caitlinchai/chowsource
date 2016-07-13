b7a7fb7c4c50fa515658669868a2b5a76fe85957
Linse Mathew <email": null>
2016-07-13 11:24:31 -0400

    Solve merge conflict on schema.

---
+++ app/models/rating.rb
@@ -0,0 +1,8 @@
+class Rating < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :recipe
+
+
+  validates :user, uniqueness: { scope: :recipe, message:"only one rating per recipe"}
+  validates :recipe, presence: true
+end
