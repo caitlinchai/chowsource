3508bfe0292c2e608ecd7f1459f0a5d868870969
Caitlin Chai <caitlinchai.2012@gmail.com>
2016-07-13 15:42:45 -0400

    Revert "edit models and migrations"

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ActiveRecord::Base
-  has_many :recipes_ingredients
-  has_many :recipes, through: :recipe_ingredients
-  validates :name, presence: true
+  belongs_to :recipe
+
+  validates :name, :measurement, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,7 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
-  has_many :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredients
+  has_many :ingredients, dependent: :destroy
   has_many :ratings, dependent: :destroy
   validates :name, :difficulty_level, :prep_time, :directions, presence: true
   validates :description, :creator, :servings, :category, presence: true
