c4766bd71a6fa0d58d9efcfe611edced1a96a62d
Brandon Wu & Caitlin Chai & Linse Mathew <pair+beewuu.caitlinchai.linsemathew@devbootcamp.com>
2016-07-13 15:46:33 -0400

    commit before rebasing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @recipes = @user.recipes || []
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :location, :about)
+  end
 end
