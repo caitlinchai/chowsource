d3ff05fa90599df7cb00f2ac6c2747b0e61cd464
Linse Mathew <email": null>
2016-07-13 11:31:26 -0400

    add database contraints to ratings and categories

--- db/migrate/20160713145628_create_ratings.rb
+++ db/migrate/20160713145628_create_ratings.rb
@@ -1,8 +1,8 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
-      t.references(:user, index: true)
-      t.references(:recipe, index: true)
+      t.references :user, index: true, null: false
+      t.references :recipe, index: true, null: false
       t.timestamps null: false
 
     end
