31569824caac37b540b0d83fece648bb6e008c0e
Linse Mathew <email": null>
2016-07-13 18:39:59 -0400

    Finish homepage logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include SessionsHelper
+  def index
+    @appetizers = Recipe.joins(:ratings).where(category_id: 1).order("ratings.stars").limit(5)
+    @soups_and_salads = Recipe.joins(:ratings).where(category_id: 2).order("ratings.stars").limit(5)
+    @main_course = Recipe.joins(:ratings).where(category_id: 3).order("ratings.stars").limit(5)
+    @dessert = Recipe.joins(:ratings).where(category_id: 4).order("ratings.stars").limit(5)
+  end
 
 end
