de1b08acb9950be8185181158e00e74ff71e8a80
Linse Mathew <email": null>
2016-07-14 15:18:06 -0400

    Add ratings by stars for recipes.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,20 +1,20 @@
 class RatingsController < ApplicationController
-  def index
-    @ingredients = Ingredient.all
-  end
-
-  def new
-    @ingredient = Ingredient.new
-  end
 
   def create
-    @ingredient = Ingredient.create!(name_params)
+    if logged_in?
+      @recipe = Recipe.find(params[:recipe_id])
+      @rating = Rating.create(recipe_id: @recipe.id, user_id: session[:user_id], stars: params[:value].to_i + 1)
+      @vote_saved = true
+      redirect_to recipe_path(@recipe)
+    else
+      redirect_to '/register'
+    end
   end
 
 private
 
 def name_params
-  params.require(:ingredient).permit(:name)
+  params.require(:rating).permit(:value)
 end
 
 end
