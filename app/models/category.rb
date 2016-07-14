76316b288f9c522ace45d3864471df4ecd85d1ad
Brandon Wu <brandon.wu72@myhunter.cuny.edu>
2016-07-14 02:07:54 -0400

    add logic to get first five recipes of a given category based on rating and add bootstrap links

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,7 @@ class Category < ActiveRecord::Base
   has_many :users, through: :recipes
   validates :name, presence: true
 
-
+  def top_five
+    self.recipes.sort{ |x, y| y.average_rating <=> x.average_rating }
+  end
 end
