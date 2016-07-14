207c962d806622d8a2032b14cac6df9b1a2ec2b0
Linse Mathew <email": null>
2016-07-14 11:42:01 -0400

    Add ability for users to upload pictures and fix seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,17 +25,17 @@ Recipe.create({name: "Carrot Coconut Soup", difficulty_level: "Expert", prep_tim
 
 Recipe.create({name: "Red Wine", difficulty_level: "Beginner", prep_time_hours: 0, prep_time_min: 20, directions: "There's a lot of stuff", description: "The crimini mushrooms and tawny port gives this Barley Mushroom Soup extra flavor. This soup is a great way to enjoy the health benefits of barley.", creator: "Grandma", servings: 4, category_id: 1, user_id: 1})
 
-Recipe.create({name: "Lentils", difficulty_level: "Intermediate", prep_time_hours: 0, prep_time_min: "30", directions: "There's a lot of stuff", description: "This tastes really, really good", creator: "Grandma", servings: 4, category_id: 2, user_id: 3})
+Recipe.create({name: "Lentils", difficulty_level: "Intermediate", prep_time_hours: 0, prep_time_min: 30, directions: "There's a lot of stuff", description: "This tastes really, really good", creator: "Grandma", servings: 4, category_id: 2, user_id: 3})
 
-Recipe.create({name: "Braised Ribs", difficulty_level: "Expert", prep_time_hours:3, prep_time_min: "40", directions: "There's a lot of stuff", description: "This tastes good", creator: "My Great-Great-Aunt", servings: 4, category_id: 3, user_id: 4})
+Recipe.create({name: "Braised Ribs", difficulty_level: "Expert", prep_time_hours:3, prep_time_min: 40, directions: "There's a lot of stuff", description: "This tastes good", creator: "My Great-Great-Aunt", servings: 4, category_id: 3, user_id: 4})
 
-Recipe.create({name: "Mushroom", difficulty_level: "Expert", prep_time_hours: 1, prep_time_min: "10", directions: "There's a lot of stuff", description: "This tastes good", creator: "Cousin, twice-removed", servings: 4, category_id: 1, user_id: 4})
+Recipe.create({name: "Mushroom", difficulty_level: "Expert", prep_time_hours: 1, prep_time_min: 10, directions: "There's a lot of stuff", description: "This tastes good", creator: "Cousin, twice-removed", servings: 4, category_id: 1, user_id: 4})
 
-Recipe.create({name: "Risotto", difficulty_level: "Beginner", prep_time_hours: 0, prep_time_min: "25", directions: "There's a lot of stuff", description: "This tastes good", creator: "Neighbor", servings: 4, category_id: 1, user_id: 1})
+Recipe.create({name: "Risotto", difficulty_level: "Beginner", prep_time_hours: 0, prep_time_min: 25, directions: "There's a lot of stuff", description: "This tastes good", creator: "Neighbor", servings: 4, category_id: 1, user_id: 1})
 
-Recipe.create({name: "Seafood Gazpacho", difficulty_level: "Beginner", prep_time_hours: 0, prep_time_min: "10", directions: "There's a lot of stuff", description: "This tastes good", creator: "my boss", servings: 4, category_id: 2, user_id: 4})
+Recipe.create({name: "Seafood Gazpacho", difficulty_level: "Beginner", prep_time_hours: 0, prep_time_min: 10, directions: "There's a lot of stuff", description: "This tastes good", creator: "my boss", servings: 4, category_id: 2, user_id: 4})
 
-Recipe.create({name: "Spicy Posole Soup", difficulty_level: "Intermediate", prep_time_hours: 2, prep_time_min: "40", directions: "There's a lot of stuff", description: "This tastes good", creator: "Mother Dearest", servings: 4, category_id: 3, user_id: 2})
+Recipe.create({name: "Spicy Posole Soup", difficulty_level: "Intermediate", prep_time_hours: 2, prep_time_min: 40, directions: "There's a lot of stuff", description: "This tastes good", creator: "Mother Dearest", servings: 4, category_id: 3, user_id: 2})
 
 RecipeIngredient.create(recipe_id: 1, ingredient_id: 1, measurement_id: 1, quantity: 2.0)
 RecipeIngredient.create(recipe_id: 1, ingredient_id: 2, measurement_id: 2, quantity: 2.0)
