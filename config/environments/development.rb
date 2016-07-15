f0f26fe7a9417548f2e7156594881d2679ec45b1
caitlinchai <caitlinchai.2012@gmail.com>
2016-07-15 16:57:52 -0400

    image magick

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,14 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+    # config.action_view.raise_on_missing_translations = true
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
